c0e1e98bd3a56b8aaaa69046cbf2e127c67576e2
Guspascad420 <Paacad@gmail.com>
2023-02-14 11:59:44 +0700

    Added HomeScreen composable

--- app/src/main/java/com/example/raion_battlepass/ui/BattlepassApp.kt
+++ app/src/main/java/com/example/raion_battlepass/ui/BattlepassApp.kt
@@ -1,19 +1,9 @@
 package com.example.raion_battlepass.ui
 
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import com.example.raion_battlepass.ui.screens.HomeScreen
 
 @Composable
 fun BattlepassApp() {
-    Surface(
-        modifier = Modifier
-            .fillMaxSize(),
-        color = MaterialTheme.colors.background
-    ) {
-
-    }
+    HomeScreen()
 }
