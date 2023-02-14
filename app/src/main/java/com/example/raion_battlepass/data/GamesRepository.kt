18a85a8b313f8b1efb693d63bc3d081f635138be
Guspascad420 <Paacad@gmail.com>
2023-02-14 12:01:24 +0700

    Added function to get all games

--- app/src/main/java/com/example/raion_battlepass/data/GamesRepository.kt
+++ app/src/main/java/com/example/raion_battlepass/data/GamesRepository.kt
@@ -5,10 +5,15 @@ import com.example.raion_battlepass.network.GamesApiService
 
 interface GamesRepository {
     suspend fun getGames(category: String, platform: String): List<Game>
+    suspend fun getAllGames(): List<Game>
 }
 
 class DefaultGamesRepository(private val gamesApiService: GamesApiService) : GamesRepository {
     override suspend fun getGames(category: String, platform: String): List<Game> {
         return gamesApiService.getGames(category, platform)
+    }
+
+    override suspend fun getAllGames(): List<Game> {
+        return gamesApiService.getAllGames()
     }
 }
