33e33389aabb9c51a48d97ab421cbef4ca4d0bdb
Guspascad420 <Paacad@gmail.com>
2023-02-14 12:03:03 +0700

    Added new fonts

--- app/src/main/java/com/example/raion_battlepass/ui/theme/Type.kt
+++ app/src/main/java/com/example/raion_battlepass/ui/theme/Type.kt
@@ -2,9 +2,16 @@ package com.example.raion_battlepass.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.raion_battlepass.R
+
+val Roboto = FontFamily(
+    Font(R.font.roboto_bold)
+)
+
 
 // Set of Material typography styles to start with
 val Typography = Typography(
