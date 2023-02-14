6546ad881d1ab94456a8887b6b19f8afae459363
Guspascad420 <Paacad@gmail.com>
2023-02-14 12:02:55 +0700

    Initial commit

--- app/src/main/java/com/example/raion_battlepass/MainActivity.kt
+++ app/src/main/java/com/example/raion_battlepass/MainActivity.kt
@@ -3,24 +3,9 @@ package com.example.raion_battlepass
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.KeyboardArrowDown
-import androidx.compose.material.icons.filled.KeyboardArrowUp
-import androidx.compose.runtime.*
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Size
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.toSize
+import com.example.raion_battlepass.ui.BattlepassApp
 import com.example.raion_battlepass.ui.theme.RaionbattlepassTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,70 +14,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             RaionbattlepassTheme {
                 // A surface container using the 'background' color from the theme
-                val options = listOf("Option 1", "Option 2", "Option 3")
-                SelectDropdown(options = options)
-
+                BattlepassApp()
             }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@OptIn(ExperimentalMaterialApi::class)
-@Composable
-fun SelectDropdown(options: List<String>) {
-    var expanded by remember { mutableStateOf(false) }
-    var selectedOption by remember { mutableStateOf("Option 1") }
-    val icon = if (expanded)
-        Icons.Filled.KeyboardArrowUp
-    else
-        Icons.Filled.KeyboardArrowDown
-
-    Column(Modifier.padding(20.dp)) {
-
-        ExposedDropdownMenuBox(
-            expanded = expanded,
-            onExpandedChange = {
-                expanded = !expanded
-            }
-        ) {
-
-            // text field
-            OutlinedTextField(
-                value = selectedOption,
-                onValueChange = {},
-                readOnly = true,
-                trailingIcon = {
-                    ExposedDropdownMenuDefaults.TrailingIcon(
-                        expanded = expanded,
-                    )
-                },
-                colors = ExposedDropdownMenuDefaults.textFieldColors(
-                    backgroundColor = Color.White,
-                ),
-                shape = RoundedCornerShape(10.dp)
-            )
-
-            DropdownMenu(
-                expanded = expanded,
-                onDismissRequest = { expanded = false }
-            ) {
-                options.forEach { option ->
-                    DropdownMenuItem(
-                        onClick = {
-                            selectedOption = option
-                            expanded = false
-                        },
-                    ) {
-                        Text(option)
-                    }
-                }
-            }
-
         }
     }
 }
@@ -101,7 +24,6 @@ fun SelectDropdown(options: List<String>) {
 @Composable
 fun DefaultPreview() {
     RaionbattlepassTheme {
-        val options = listOf("Option 1", "Option 2", "Option 3")
-        SelectDropdown(options = options)
+
     }
 }
